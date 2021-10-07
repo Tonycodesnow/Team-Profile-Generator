9e519ff48e2df3fdb273ede98ffb85c548c03075
Ant <huertinez.family@outlook.com>
2021-10-06 23:26:53 -0700

    refactored tests

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,23 +1,21 @@
 const Employee = require("../lib/Employee");
 
 describe("create a Employee", () => {
-    const myEmployee = new Employee ("Sam", 123, "Sam1@mymail.com");
+  const myEmployee = new Employee("Sam", 123, "Sam1@mymail.com");
 
-    test("check name", () => {
-        expect (myEmployee.getName()).toEqual("Sam");
-    });
-    
-    test("check Id", () => {
-        expect (myEmployee.getId()).toEqual(123);
+  test("check name", () => {
+    expect(myEmployee.getName()).toEqual("Sam");
+  });
 
-    });
-    
-    test("check Email", () => {
-        expect (myEmployee.getEmail()).toEqual("Sam1@mymail.com");
+  test("check Id", () => {
+    expect(myEmployee.getId()).toEqual(123);
+  });
 
-    });
-    
-    test("check Role", () => {
-        expect (myEmployee.getRole()).toEqual('Employee');
-    })
+  test("check Email", () => {
+    expect(myEmployee.getEmail()).toEqual("Sam1@mymail.com");
+  });
+
+  test("check Role", () => {
+    expect(myEmployee.getRole()).toEqual("Employee");
+  });
 });
--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,25 +1,25 @@
 const Intern = require("../lib/Intern");
 
 describe("create an Intern", () => {
-    const myIntern = new Intern ("Maya", 123, "Maya1@mymail.com", 'JFK');
+  const myIntern = new Intern("Maya", 123, "Maya1@mymail.com", "JFK");
 
-    test("check name", () => {
-        expect (myIntern.name).toEqual("Maya");
-    });
-        
-    test("check Id", () => {
-        expect(myIntern.getId()).toEqual(123);
-      });
-    
-      test("check Email", () => {
-        expect(myIntern.getEmail()).toEqual("Maya1@mymail.com");
-      });
-    
-      test("check School", () => {
-        expect(myIntern.getSchool()).toEqual('JFK');
-      });
-    
-      test("check Role", () => {
-        expect(myIntern.getRole()).toEqual("Intern");   
-      })
+  test("check name", () => {
+    expect(myIntern.name).toEqual("Maya");
+  });
+
+  test("check Id", () => {
+    expect(myIntern.getId()).toEqual(123);
+  });
+
+  test("check Email", () => {
+    expect(myIntern.getEmail()).toEqual("Maya1@mymail.com");
+  });
+
+  test("check School", () => {
+    expect(myIntern.getSchool()).toEqual("JFK");
+  });
+
+  test("check Role", () => {
+    expect(myIntern.getRole()).toEqual("Intern");
+  });
 });
