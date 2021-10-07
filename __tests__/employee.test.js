8a8b54dd7e5357378edb196b69da4b99f1952e6c
Ant <huertinez.family@outlook.com>
2021-10-06 22:27:57 -0700

    finished intern tests

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,33 +1,23 @@
 const Employee = require("../lib/Employee");
 
 describe("create a Employee", () => {
+    const myEmployee = new Employee ("Sam", 123, "Sam1@mymail.com");
 
     test("check name", () => {
-        const myEmployee = new Employee ("Sam", 123, "Sam1@mymail.com");
-        
         expect (myEmployee.getName()).toEqual("Sam");
     });
     
     test("check Id", () => {
-        const myEmployee = new Employee ("Sam", 123, "Sam1@mymail.com");
-        
         expect (myEmployee.getId()).toEqual(123);
-        
+
     });
-    
     
     test("check Email", () => {
-        const myEmployee = new Employee ("Sam", 123, "Sam1@mymail.com");
-        
         expect (myEmployee.getEmail()).toEqual("Sam1@mymail.com");
-        
+
     });
     
-    
     test("check Role", () => {
-        const myEmployee = new Employee ("Sam", 123, "Sam1@mymail.com");
-        
         expect (myEmployee.getRole()).toEqual('Employee');
-    
     })
 });
